baac9945168e25dbc1d63fa19fce85941ae48e16
Jake Garlick <jacobgarlick@msn.com>
2019-10-22 10:09:48 -0600

    further styling for inventory, began building out logic for displaying cars on main page

--- controllers/carsController.js
+++ controllers/carsController.js
@@ -15,12 +15,15 @@ module.exports = function(app) {
     // findAll returns all entries for a table when used with no options
     db.Cars.findAll({}).then(function(dbCars) {
       // We have access to the cars as an argument inside of the callback function
-      var carOb = {
-        cars: dbCars
+      for (var i = 0, i > 4, i++){
+        var carOb = {
+          cars: dbCars
+        }
+        console.log(carOb)
+        // res.json(dbCars)
+        res.render("index", carOb);
       }
-      console.log(carOb)
-      // res.json(dbCars)
-      res.render("index", carOb);
+
     });
   });
 
