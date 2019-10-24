c46a21a6ab87a9cd5d09ff43aeebb12a4772d20c
Casey Levin <clevin8080@yahoo.com>
2019-10-24 08:44:27 -0600

    css styling

--- models/usersModels.js
+++ models/usersModels.js
@@ -1,62 +1,60 @@
 module.exports = function(sequelize, DataTypes) {
-  var Users = sequelize.define("Users", {
-    user_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        // notNull: true,
-        // notEmpty: true,
-        len: [1, 200]
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        // notNull: true,
-        // notEmpty: true,
-        len: [1, 200]
-      }
-    },
-    favorite_color: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      validate: {
-        // notNull: true,
-        // notEmpty: true,
-        len: [1, 200]
-      }
-    },
-    favorite_type: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      validate: {
-        // notNull: true,
-        // notEmpty: true,
-        len: [1, 200]
-      }
-    },
-    favorite_make: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      validate: {
-        // notNull: true,
-        // notEmpty: true,
-        len: [1, 200]
-      }
-    }
-    
-  }, {
-    timestamps: false
-  });
+    var Users = sequelize.define("Users", {
+        user_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // notNull: true,
+                // notEmpty: true,
+                len: [1, 200]
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // notNull: true,
+                // notEmpty: true,
+                len: [1, 200]
+            }
+        },
+        favorite_color: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                // notNull: true,
+                // notEmpty: true,
+                len: [1, 200]
+            }
+        },
+        favorite_type: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                // notNull: true,
+                // notEmpty: true,
+                len: [1, 200]
+            }
+        },
+        favorite_make: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                // notNull: true,
+                // notEmpty: true,
+                len: [1, 200]
+            }
+        }
 
-  Users.associate = function(models) {
-    // Associating Author with Posts
-    // When an Author is deleted, also delete any associated Posts
-    Users.hasMany(models.Cars, {
-      onDelete: "cascade"
+    }, {
+        timestamps: false
     });
-  };
-
-  return Users;
+    Users.associate = function(models) {
+        // Associating Author with Posts
+        // When an Author is deleted, also delete any associated Posts
+        Users.hasMany(models.Cars, {
+            onDelete: "cascade"
+        });
+    };
+    return Users;
 };
